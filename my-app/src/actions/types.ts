c11dcfd024497cfe0f02844df43ad6314c4bb767
zoverlvx <zoverhulser@gmail.com>
2020-05-03 19:18:03 -0400

    Changes name of main app folder.

---
+++ my-app/src/actions/types.ts
@@ -0,0 +1,14 @@
+import {
+    FetchTodosAction,
+    DeleteTodoAction
+} from "./todos";
+
+export enum ActionTypes {
+    fetchTodos,
+    deleteTodo
+    }
+
+export type Action = (
+    FetchTodosAction 
+    | DeleteTodoAction
+)
