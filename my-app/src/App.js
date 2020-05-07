72791fd7f4ee333f62a8c31d5926fa51e21d0518
zoverlvx <zoverhulser@gmail.com>
2020-05-07 00:59:40 -0400

    Creates todo app with React classes and binds methods to constructor.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,56 +1,128 @@
 import React, {Component} from "react";
 import "./App.css";
 
-class App extends Component {
-	constructor() {
-		super();
+class TaskForm extends Component {
+	constructor(props) {
+		super(props);
 		this.state = {
-			todos: [
-				{
-					text: "Make bed",
-					completed: false
-				},
-				{
-					text: "Read",
-					completed: false
-				}
-			],
-			todo: ""
+			task: ""
 		};
-		this.createTodo = this.createTodo.bind(this);
-		this.addTodo = this.addTodo.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+		this.handleChange = this.handleChange.bind(this);
+	}
+	
+	handleSubmit(e) {
+		e.preventDefault();
+		if (this.state.task.length) {
+			this.props.addTask(this.state.task);
+			this.setState({task: ""});
+		} else {
+			alert("Please, add a task.");
+		}
 	}
 
 	handleChange(e) {
 		this.setState({
-			todo: e.target.value
-		});
-	}
-	
-	addTodo(e) {
-		e.preventDefault();
-		const newTodo = {text: this.state.todo, completed: false};
-		this.setState({
-			todos: [...this.state.todos, newTodo],
-			todo: ""
+			task: e.target.value
 		});
 	}
 
-	createTodo(todo, index) {
+	render () {
+		return (
+			<form>
+				<input 
+					value={this.state.task} 
+					onChange={this.handleChange}
+				/>
+				<button onClick={this.handleSubmit}>Add</button>
+			</form>
+		);
+	}
+}
+
+class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			tasks: [
+				{	
+					id: 1,
+					text: "Make bed",
+					completed: false
+				},
+				{	
+					id: 2,
+					text: "Read",
+					completed: false
+				}
+			],
+		};
+		this.createTaskList = this.createTaskList.bind(this);
+		this.addTaskToDatabase = this.addTaskToDatabase.bind(this);
+		this.deleteTaskFromDatabase = this.deleteTaskFromDatabase.bind(this);
+		this.toggleTask = this.toggleTask.bind(this);
+	}
+
+	toggleTask(chosenId) {
+		this.setState({
+			tasks: this.state.tasks.map(function(item) {
+				if (item.id === chosenId) {
+					return {
+						...item,
+						completed: !item.completed
+					};
+				}
+				return item;
+			})
+		});
+	}
+
+	//task: string
+	addTaskToDatabase(task) {
+		this.setState({ 
+			tasks: [
+				...this.state.tasks, 
+				{
+					id: (new Date()).getTime(),
+					text: task, 
+					completed: false
+				}
+			]
+		});
+	}
+
+	// id: number
+	deleteTaskFromDatabase(id) {
+		const tasks = [...this.state.tasks];
+		const index = tasks.map(item => item.id).indexOf(id);
+		tasks.splice(index, 1);
+		this.setState({tasks});
+	}
+
+	// task: object {id: number, task: string, completed: boolean}
+	createTaskList(task) {
 		const todoStyle = {
 			display: "inline-block",
-			textDecoration: todo.completed
+			textDecoration: task.completed
 				? "line-through"
 				: ""
 		};
 
 		return (
-			<div key={index}>
+			<div key={task.id}>
 				<div
+					onClick={() => this.toggleTask(task.id)}
 					style={todoStyle}
 				>
-					{todo.text}
+					{task.text}
 				</div>
+				<button 
+					onClick={
+						() => this.deleteTaskFromDatabase(task.id)
+					}
+				>
+					X
+				</button>
 			</div>
 		);
 	}
@@ -58,7 +130,8 @@ class App extends Component {
 	render() {
 		return (
 			<>
-				{this.state.todos.map(this.createTodo)}
+				{this.state.tasks.map(this.createTaskList)}
+				<TaskForm addTask={this.addTaskToDatabase}/>
 			</>
 		);
 	}
